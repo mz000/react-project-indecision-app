b4f6b77c262b4ed6468e8b5e9a8451445795ee8e
MbZ <amobarez00@gmail.com>
2020-07-28 21:00:18 +0430

    edit app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,8 @@
 console.log("app.js is running")
+
+// JSX - JavaSript XML
+
+template = template = React.createElement("p", {id: "someid"}, "this is template")
+appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
