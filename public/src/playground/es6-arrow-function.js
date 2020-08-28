1bf987e7be8772b5fa19d406ccbd37014479ddf5
MbZ <amobarez00@gmail.com>
2020-08-28 19:36:09 +0430

    practicing with react - some codes removed

---
+++ public/src/playground/es6-arrow-function.js
@@ -0,0 +1,12 @@
+
+// es5 function
+const square = function(x) {
+    return x*x;
+}
+
+// es6 arrow function
+const squareArrow = (x) => {
+    return x*x;
+}
+
+console.log(squareArrow(6))
---
+++ public/src/playground/es6-arrow-function2.js
@@ -0,0 +1,32 @@
+// argument objects - no longer bound with arrow functions
+
+const add = (a, b) => {
+   // console.log(arguments)
+    return a+b
+} 
+// we will get an error here 
+
+
+// this keyword - no longer bound
+const user = {
+    name: 'andrew',
+    cities: ["new york", "philadelphia", "dublin"],
+    printPlcaeslived() {
+        return this.cities.map((city) => this.name + " has lived in " + city);
+    }
+}
+console.log(user.printPlcaeslived())
+
+// challenge area 
+
+const multiplier = {
+    numbers : [2, 4, 6, 8, 10, 12],
+    multipyBy: 4,
+    multipy() {
+        return this.numbers.map((num) => num * this.multipyBy)
+    }
+}
+ 
+console.log(multiplier.multipy())
+
+ 
