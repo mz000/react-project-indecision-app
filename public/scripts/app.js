1857bf3024ceb14b4b9ddd6c7e4d763556e07c42
MbZ <amobarez00@gmail.com>
2020-09-01 17:51:48 +0430

    arrays in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,67 +8,108 @@ var app = {
 
 var formSubmit = function formSubmit(e) {
     e.preventDefault();
-
-    console.log(e.type);
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = "";
+        console.log(app.options);
+    } else {
+        console.log("empty fields!!");
+    }
+    renderTemplate();
 };
 
-var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        "Welcome to my site!"
-    ),
-    React.createElement(
-        "p",
-        null,
-        "Home"
-    ),
-    app.subtitle && React.createElement(
-        "p",
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        "a",
-        { href: "https://www.google.com/" },
-        "Google"
-    ),
-    React.createElement("br", null),
-    React.createElement(
-        "a",
-        { href: "https://www.bing.com/" },
-        "Bing"
-    ),
-    React.createElement("br", null),
-    app.subtitle && React.createElement(
-        "p",
-        null,
-        "Title: ",
-        app.title
-    ) && React.createElement(
-        "p",
-        null,
-        "Subtitle: ",
-        app.subtitle
-    ),
-    React.createElement(
-        "p",
-        null,
-        app.options.length > 0 ? "here are your options: " + app.options : "there is no option"
-    ),
-    React.createElement(
-        "form",
-        { onSubmit: formSubmit },
-        React.createElement("input", { type: "text", name: "option" }),
-        React.createElement(
-            "button",
-            null,
-            "Add option"
-        )
-    )
-);
+var resetSubmit = function resetSubmit(e) {
+    //e.target.elements.option.value = "";
+    app.options = [];
+    renderTemplate();
+};
 
 var approot = document.getElementById('app');
-ReactDOM.render(template, approot);
+var renderTemplate = function renderTemplate() {
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            "Welcome to my site!"
+        ),
+        React.createElement(
+            "p",
+            null,
+            "Home"
+        ),
+        app.subtitle && React.createElement(
+            "p",
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            "a",
+            { href: "https://www.google.com/" },
+            "Google"
+        ),
+        React.createElement("br", null),
+        React.createElement(
+            "a",
+            { href: "https://www.bing.com/" },
+            "Bing"
+        ),
+        React.createElement("br", null),
+        app.subtitle && React.createElement(
+            "p",
+            null,
+            "Title: ",
+            app.title
+        ) && React.createElement(
+            "p",
+            null,
+            "Subtitle: ",
+            app.subtitle
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length > 0 ? "here are your options: " + app.options : "there is no option"
+        ),
+        React.createElement(
+            "h3",
+            null,
+            app.options.length
+        ),
+        [React.createElement(
+            "p",
+            { key: "1" },
+            "a"
+        ), React.createElement(
+            "p",
+            { key: "2" },
+            "b"
+        ), React.createElement(
+            "p",
+            { key: "3" },
+            "c"
+        )],
+        React.createElement(
+            "form",
+            { onSubmit: formSubmit },
+            React.createElement("input", { type: "text", name: "option" }),
+            React.createElement(
+                "button",
+                null,
+                "Add option"
+            )
+        ),
+        React.createElement("br", null),
+        React.createElement(
+            "button",
+            { onClick: resetSubmit },
+            "Reset"
+        )
+    );
+
+    ReactDOM.render(template, approot);
+};
+
+renderTemplate();
--- public/src/formsAndInputs.js
+++ public/src/formsAndInputs.js
@@ -6,30 +6,55 @@ let app = {
 
 const formSubmit = (e) => {
     e.preventDefault()
-
-    console.log(e.type)
+    const option = e.target.elements.option.value;
+    if(option)
+    {
+        app.options.push(option)
+        e.target.elements.option.value = "";
+        console.log(app.options)
+    } else {
+        console.log("empty fields!!")
+    }
+    renderTemplate();
 }
 
-let template = (
-    <div>
-        <h1>Welcome to my site!</h1>
-        <p>Home</p>
-        {app.subtitle && <p>{app.subtitle}</p>}
+const resetSubmit = (e) => {
+    //e.target.elements.option.value = "";
+    app.options = [];
+    renderTemplate();
+}
 
-        <a href="https://www.google.com/">Google</a>
-        <br></br>
-        <a href="https://www.bing.com/">Bing</a>
-        <br></br>
-
-        {app.subtitle && <p>Title: {app.title}</p> && <p>Subtitle: {app.subtitle}</p>}
-        <p>{app.options.length >0 ? "here are your options: " + app.options : "there is no option"}</p>
-
-        <form onSubmit={formSubmit}>
-            <input type="text" name="option"/>
-            <button>Add option</button>
-        </form>
-
-    </div>);
-
-    const approot = document.getElementById('app');
+const approot = document.getElementById('app');
+const renderTemplate = () => {
+    let template = (
+        <div>
+            <h1>Welcome to my site!</h1>
+            <p>Home</p>
+            {app.subtitle && <p>{app.subtitle}</p>}
+    
+            <a href="https://www.google.com/">Google</a>
+            <br></br>
+            <a href="https://www.bing.com/">Bing</a>
+            <br></br>
+    
+            {app.subtitle && <p>Title: {app.title}</p> && <p>Subtitle: {app.subtitle}</p>}
+            <p>{app.options.length >0 ? "here are your options: " + app.options : "there is no option"}</p>
+            <h3>{app.options.length}</h3>
+            {
+                [<p key="1">a</p>, <p key="2">b</p>, <p key="3">c</p>]
+            }
+            <form onSubmit={formSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>  
+            </form>
+            <br></br>
+            
+            <button onClick={resetSubmit}>Reset</button>
+    
+        </div>);
+    
     ReactDOM.render(template, approot);
+}
+
+renderTemplate();
+
